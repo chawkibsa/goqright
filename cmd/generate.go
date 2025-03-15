8f0a80c26977007e8e8985136f7b72bf1d2fa0d8
chiao7b <86046707+chawkibsa@users.noreply.github.com>
2025-03-15 07:08:56 +0000

    feature: add goqright generate subcommand

---
+++ cmd/generate.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// generateCmd represents the generate command
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates a predefined automated task",
+	Long: `The "generate" command runs a predefined automated task designed for a specific use case. 
+	
+This command must be extended with extra flags to include a custom logic.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("generate called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// generateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
